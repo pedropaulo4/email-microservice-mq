2389019266bad299f93dadb0af87afb981963d6d
Pedro Paulo <pedropaulo19977@gmail.com>
2025-01-11 15:29:14 -0300

    init commit

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,58 @@
+package com.ms.email.services;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    JavaMailSender emailSender;
+
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel email) {
+        email.setSendDateEmail(LocalDateTime.now());
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(email.getEmailFrom());
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+            emailSender.send(message);
+
+            email.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            email.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(email);
+        }
+    }
+
+
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return  emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId) {
+        return emailRepository.findById(emailId);
+    }
+
+}
